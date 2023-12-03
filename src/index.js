faae1f2bfe625294f18f5c055266d6fc3a43fbf6
Andrija Lazic <lazic.andrija223@gmail.com>
2023-12-03 23:50:31 +0100

    Enabled bot to receive messages from users

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const {Client,IntentsBitField}=require("discord.js")
+require('dotenv').config();//loads in .env config which can be acessed by process.env
 
 const client=new Client({
     intents:[
@@ -8,3 +9,13 @@ const client=new Client({
         IntentsBitField.Flags.MessageContent,
     ]
 })
+
+client.on("ready",(c)=>{
+    console.log("The bot "+c.user.tag+" is ready.")
+})
+
+client.on("messageCreate",(message)=>{
+    console.log(message.content)
+})
+
+client.login(process.env.TOKEN)
